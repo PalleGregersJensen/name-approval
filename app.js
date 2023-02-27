7fdc529bb94b4cae994aded1c77b4849cea0f084
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-02-27 12:29:12 +0100

    Fungerer stadig ikke rigtigt

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 "use strict"
 console.log("Javascript kører!")
 
-let fullname = 
+let fullname = fullname
+
+function buttonClick() {
+document.querySelector("button").addEventListener("click", showName)
+}
+
+function showName() { 
+    document.querySelector("button onclick").removeClassList("hidden");
+    if (fullname = Palle) {
+        niceName()
+    }else if (fullname = Sarah) {
+        okName()
+    }else if (fullname = Nikolaj) {
+        okName()
+    }else if (fullname = Jakob) {
+        okName()
+    } else { 
+        badName()
+    }
+};

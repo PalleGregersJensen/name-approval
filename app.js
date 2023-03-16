9adb07ad57d80daaae5a527c06883c082a998d2b
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-03-16 14:40:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 "use strict"
 console.log("Javascript kører!")
 
+let firstName = "Louise";
+let lastName = "Hansen";
 
-function buttonClick(){
-  console.log("buttonclick");
-  document.querySelector("#myButton").addEventListener("click", showName);
-}
 
-buttonClick();
+// Nedenstående to måder kan man sætte variabler sammen på:
+// let fullName = `${firstName} ${lastName}`;
 
- function showName() {
-     console.log("vis navn")
-   document.querySelector("#niceName").classList.remove("hidden");
-   document.querySelector("#niceName").classList.add("niceName");
-    // if (name = Palle)
+let fullName = firstName +" "+ lastName
+
+
+console.log(fullName);
+
+
+// function buttonClick(){
+  // console.log("buttonclick");
+  // document.querySelector("#myButton").addEventListener("click", showName);
+// }
+
+// buttonClick();
+
+//  function showName() {
+    //  console.log("vis navn")
+  //  document.querySelector("#niceName").classList.remove("hidden");
+  //  document.querySelector("#niceName").classList.add("niceName");
+    // if (name = )
         // removeClassList("Hidden");
-    // }else if (name = Sarah) {
-    // }else if (name = Nikolaj) {
-    // }else if (name = Jakob) {
+    // }else if (name = ) {
+    // }else if (name = ) {
+    // }else if (name = ) {
     // } else { 
     // }
-}
+// }
